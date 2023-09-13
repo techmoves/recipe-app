3e4b1b0fc605f560233982d2991cac2fc132c209
David Katende <katendedavidjoga@gmail.com>
2023-09-13 16:08:26 +0300

    Add recipe migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_102303) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_123257) do
+  create_table "recipe_foods", force: :cascade do |t|
+    t.string "quantity"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "preparation_time"
+    t.string "cooking_time"
+    t.boolean "public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
@@ -24,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_102303) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipe_foods", "recipe"
 end
