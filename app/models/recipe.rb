f3029e69ad99978d955461ec6871c21848db790a
David Katende <katendedavidjoga@gmail.com>
2023-09-13 18:27:52 +0300

    Add recipe and recipe food models

---
+++ app/models/recipe.rb
@@ -0,0 +1,11 @@
+class Recipe < ApplicationRecord
+    has_many :recipe_foods
+    has_many :foods, through: :recipe_foods
+
+    validates :name, presence: true
+    validates :preparation_time, presence: true
+    validates :cooking_time, presence: true
+    validates :description, presence: true
+    validates :public, presence: true
+
+end
