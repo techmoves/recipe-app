eb98ec7d918882f18b3b815e19a68ac5b79cffde
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-09-13 17:14:07 +0300

    Add inventories, inventory_foods tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_132418) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_13_140924) do
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_inventories_on_user_id"
+  end
+
+  create_table "inventory_foods", force: :cascade do |t|
+    t.string "quantity"
+    t.integer "inventory_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
+  end
+
   create_table "recipe_foods", force: :cascade do |t|
     t.string "quantity"
     t.integer "recipe_id"
@@ -27,6 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_132418) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,5 +60,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_132418) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "inventories", "users"
+  add_foreign_key "inventory_foods", "inventories"
   add_foreign_key "recipe_foods", "recipe"
+  add_foreign_key "recipes", "users"
 end
