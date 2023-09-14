8262d34549f0d61e3a233ab397bd22c413f531d7
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-09-14 17:21:59 +0300

    Add inventory show page

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -3,6 +3,10 @@ class InventoriesController < ApplicationController
     @inventories = current_user.inventories
   end
 
+  def show
+    @inventory = current_user.inventories.find(params[:id])
+  end
+
   def destroy
     @inventory = current_user.inventories.find(params[:id])
     @inventory.destroy
