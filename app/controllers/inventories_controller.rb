66f4d822e1e85c08efbbc9585f3fc8c98c8d91d4
techmoves <muhndlovu28@gmail.com>
2023-09-14 11:03:50 +0200

    Add inventories controller and delete method

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,5 +1,9 @@
 class InventoriesController < ApplicationController
 
+    def index
+    @inventories = current_user.inventories
+    end
+
      def destroy
     @inventory = current_user.inventories.find(params[:id])
     @inventory.destroy
