fda87e34e2578e9bba23ee0f584ae7822f9949b7
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-09-16 22:19:54 +0300

    Fix failing public_recipes

--- spec/requests/public_recipes_spec.rb
+++ spec/requests/public_recipes_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'PublicRecipes', type: :request do
+  before(:each) do
+    user = User.create(name: 'test', email: 'scacd@ffs.d', password: 'password')
+    user.recipes.create(
+      name: 'wqv',
+      preparation_time: '1 hour',
+      cooking_time: '1 hour',
+      public: true,
+      description: 'My Description'
+    )
+    login_as(user, scope: :user)
+  end
+
   describe 'GET /index' do
     it 'returns http success' do
       get '/public_recipes/index'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/public_recipes/show'
       expect(response).to have_http_status(:success)
     end
   end
