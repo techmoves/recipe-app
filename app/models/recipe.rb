98fed3b95f570e4c88d2f32f6f3cb838c31b8b75
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-09-16 16:14:56 +0300

    Fix Linter errors caused by merge conflict
    Co-authored-by: David Katende <katendedavidjoga@gmail.com>

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,7 +5,6 @@ class Recipe < ApplicationRecord
 
   attribute :public, :boolean, default: false
 
-
   validates :name, presence: true
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
